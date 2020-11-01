c78f0593914b986fed1122f1a45d0991feb9397c
winpasit <42579006+winpasit@users.noreply.github.com>
2020-11-01 20:01:43 +0700

    bug fixed simple merger

--- esra/mergers/simple_merger.py
+++ esra/mergers/simple_merger.py
@@ -131,12 +131,11 @@ def merging_to_kg(list_triples,existing_kg):
     for paper_id,triples in list_triples:
         for triple in triples:
             check_existing,idx = existing_in_kg(triple,existing_kg)
-        if check_existing == True:
-            existing_kg[idx][1].add(paper_id)
-        else:
-            existing_kg.append([triple,set([paper_id])])
+            if check_existing == True:
+                existing_kg[idx][1].add(paper_id)
+            else:
+                existing_kg.append([triple,set([paper_id])])
     return existing_kg
-
 
 
 #if you want to restart the flow please uncomment and run this code below:
