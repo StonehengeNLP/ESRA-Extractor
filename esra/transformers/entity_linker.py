5f1c77924297acc9ea6a9f1ef2bafcedf3830e2c
Peeranuth Kehasukcharoen <peeranuth.k@gmail.com>
2020-11-08 01:03:38 +0700

    Change: more on entity linker

--- esra/transformers/entity_linker.py
+++ esra/transformers/entity_linker.py
@@ -112,5 +112,77 @@ class Entity_Linker:
     self.model.eval()
 
     # def __init__(self):
+
+    def generate_emb_vector(self, entity_name):
+        """
+            Return entity name embedding numpy array
+        """
+        tokens = self.tokenizer.encode(entity_name, return_tensors='pt')
+        with torch.no_grad():
+            model_output = self.model(tokens)
+        hidden_states = model_output[2]
+        # create sentence embedding frim hidden states 
+        out = hidden_states[-2][0].mean(dim=0)
+        return out.detach().numpy()
     
+    def _save_tree(self, f_name='vec.ann'):
+        """
+            Save vector tree
+        """
+        t = AnnoyIndex(DIMENTIONS, 'angular') # init angular(cosine) index
+        
+        for i,v in enumerate(vectors):
+            t.add_item(i,v)
+        
+        t.build(NUM_TREE)
+        t.save(f_name)
+    
+    def cosine_similarity(self, a, b):
+        na = np.linalg.norm(a)
+        nb = np.linalg.norm(b)
+        return (a @ b.T)/(na*nb)
+
+    def vector_similarity(self, document_entities, f_name='vec.ann'):
+        """
+            generate index of KG entity that doc link to
+            if unlinkable then reptresent with -1
+        """
+        # load tree
+        if not os.path.exists(f_name):
+            raise FileNotFoundError("Missing tree file")
+        t = AnnoyIndex(DIMENTIONS, 'angular')
+        t.load(f_name)
+
+        # store index
+        kg_indexes = []
+
+        # for each document entity
+        for entity in document_entities:
+            input_vector = self.generate_vector(entity[1])
+            k_indexes = t.get_nns_by_vector(
+                input_vector,
+                NUM_NEIGHBOR,
+                search_k=-1,
+                include_distances=False
+            )
+            # check cosine similarity
+            max_cos_sim = self.cosine_similarity(
+                input_vector,
+                np.array(t.get_item_vector(k_indexes[0]))
+            )
+            if max_cos_sim < SIMILARITY_THRESHOLD: 
+                kg_indexes.append(-1)
+            else:
+                # select node with max cosine similarity
+                kg_indexes.append(k_indexes[0])
+
+        return kg_indexes
+
+    def word_structure_matching(self, document_entities):
+        
+        return None
+    
+    def doc_entity_linking(self, document_entities):
+
+        emb_idx = self.vector_similarity(document_entities)
         
