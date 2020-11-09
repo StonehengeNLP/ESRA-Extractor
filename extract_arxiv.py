e68c8b03d133c9c048681dcaeec961593d1848cb
tongplw <bangkok.tong@gmail.com>
2020-11-09 02:16:08 +0000

    added arxiv_cscl_5000.pickle

--- extract_arxiv.py
+++ extract_arxiv.py
@@ -7,19 +7,20 @@ import pandas as pd
 from esra.extractors import ESRAE 
 from esra.utils import nlp_split
 
-for filename in glob.glob('arxiv/*.csv')[2:3]:
-    print(filename)
-    
-    df = pd.read_csv(filename)
-    abstracts = df.abstract.to_list()
-    r = ESRAE.extract(abstracts)
-    
-    # with open('./pickle/arxiv_cscl_200.pickle', 'rb') as f:
-    #     r = pickle.load(f)
-    
-    # label their id
-    for doc, id in zip(r, df.id):
-        doc['id'] = id
+for filename in glob.glob('arxiv/*.csv'):
+    if '5000' in filename:
+        print(filename)
+        
+        df = pd.read_csv(filename)
+        abstracts = df.abstract.to_list()
+        r = ESRAE.extract(abstracts)
+        
+        # with open('./pickle/arxiv_cscl_200.pickle', 'rb') as f:
+        #     r = pickle.load(f)
+        
+        # label their id
+        for doc, id in zip(r, df.id):
+            doc['id'] = id
 
-    with open(f'pickle/{filename[6:-4]}.pickle', 'wb') as f:
-        pickle.dump(r, f)
+        with open(f'pickle/{filename[6:-4]}.pickle', 'wb') as f:
+            pickle.dump(r, f)
