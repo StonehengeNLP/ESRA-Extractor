a51f8b53626707d98dc8b688cb0675179f6514fd
Peeranuth Kehasukcharoen <peeranuth.k@gmail.com>
2020-12-06 14:39:59 +0700

    Change: Linker only create vector now

--- esra/transformers/entity_linker.py
+++ esra/transformers/entity_linker.py
@@ -2,110 +2,18 @@
 
 import torch
 import numpy as np
-import os
-import pickle
+
 try:
     from transformers import *
 except:
     raise ImportError("Unable to load transformers")
 
-try:
-    from annoy import AnnoyIndex
-except:
-    raise ImportError("Unable to load annoy")
-
-
-# SciBERT pretrained model folder is in transformers/
-
-# init Scibert model
-# Fix transformers package version to 2.11.0 pls
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-tokenizer = BertTokenizer.from_pretrained(
-    'allenai/scibert_scivocab_uncased'
-    )
-model = BertModel.from_pretrained(
-    'allenai/scibert_scivocab_uncased',
-    output_hidden_states=True
-    ).to(device)
-model.eval()
-
-# Constant var
-DIMENTIONS = 768
-NUM_TREE = 10
-NUM_NEIGHBOR = 3
-SIMILARITY_THRESHOLD = 0.9
-pickle_path = '../../pickle/vectors_9.pickle'
-
-def generate_vector(entity_name:str) -> np.ndarray:
-    global device
+class Entity_Embedder:
     """
-        Return entity name embedding numpy array
+        A class for embedding entity name to embedding vector
     """
-    with torch.no_grad():
-        tokens = tokenizer.encode(entity_name, return_tensors='pt').to(device)
-        model_output = model(tokens)
-    hidden_states = model_output[2]
-    # create sentence embedding frim hidden states 
-    out = hidden_states[-2][0].mean(dim=0)
-    return out.cpu().detach().numpy()
-    
-def save_tree(vectors, f_name='vec.ann'):
-    """
-        Save vector tree
-    """
-    t = AnnoyIndex(DIMENTIONS, 'angular') # init angular(cosine) index
-    
-    for i,v in enumerate(vectors):
-        t.add_item(i,v)
-    
-    t.build(NUM_TREE)
-    t.save(f_name)
 
-def cosine_similarity(a,b):
-    na = np.linalg.norm(a)
-    nb = np.linalg.norm(b)
-    return (a @ b.T)/(na*nb)     
-
-def doc_kg_linking(document_entities, f_name='vec.ann'):
-    """
-        generate index of KG entity that doc link to
-        if unlinkingable then reptresent with -1
-    """
-    # load tree
-    if not os.path.exists(f_name):
-        raise FileNotFoundError("Missing tree file")
-    t = AnnoyIndex(DIMENTIONS, 'angular')
-    t.load(f_name)
-
-    # store index
-    kg_indexes = []
-
-    # for each document entity
-    for entity in document_entities:
-        input_vector = generate_vector(entity[1])
-        k_indexes = t.get_nns_by_vector(
-            input_vector,
-            NUM_NEIGHBOR,
-            search_k=-1,
-            include_distances=False
-        )
-        # check cosine similarity
-        max_cos_sim = cosine_similarity(
-            input_vector,
-            np.array(t.get_item_vector(k_indexes[0]))
-        )
-        if max_cos_sim < SIMILARITY_THRESHOLD: 
-            kg_indexes.append(-1)
-        else:
-            # select node with max cosine similarity
-            kg_indexes.append(k_indexes[0])
-
-    return kg_indexes
-
-
-# change to class
-class Entity_Linker:
-    
+    # use SciBERT
     tokenizer = BertTokenizer.from_pretrained(
         'allenai/scibert_scivocab_uncased'
     )
@@ -115,12 +23,8 @@ class Entity_Linker:
     )
     model.eval()
 
-    def __init__(self, pickle_path):
-        with open(pickle_path, 'rb') as f:
-            vec_n_ent =  pickle.load(f)
-        self.vectors = vec_n_ent['vectors']
-        self.entities = vec_n_ent['entities']
-        
+    def __init__(self):
+        pass
 
     def generate_emb_vector(self, entity_name):
         """
@@ -133,73 +37,3 @@ class Entity_Linker:
         # create sentence embedding frim hidden states 
         out = hidden_states[-2][0].mean(dim=0)
         return out.detach().numpy()
-    
-    def _save_tree(self, f_name='vec.ann'):
-        """
-            Save vector tree
-        """
-        t = AnnoyIndex(DIMENTIONS, 'angular') # init angular(cosine) index
-        
-        for i,v in enumerate(vectors):
-            t.add_item(i,v)
-        
-        t.build(NUM_TREE)
-        t.save(f_name)
-    
-    def _save_pickle(self, pickle_path):
-        with open(pickle_path, 'rb') as f:
-            save_dict = dict(
-                vectors = self.vectors,
-                entities = self.entities 
-            )
-            pickle.dump(f)
-    
-    def cosine_similarity(self, a, b):
-        na = np.linalg.norm(a)
-        nb = np.linalg.norm(b)
-        return (a @ b.T)/(na*nb)
-
-    def vector_similarity(self, document_entities, f_name='vec.ann'):
-        """
-            generate index of KG entity that doc link to
-            if unlinkable then reptresent with -1
-        """
-        # load tree
-        if not os.path.exists(f_name):
-            raise FileNotFoundError("Missing tree file")
-        t = AnnoyIndex(DIMENTIONS, 'angular')
-        t.load(f_name)
-
-        # store index
-        kg_indexes = []
-
-        # for each document entity
-        for entity in document_entities:
-            input_vector = self.generate_vector(entity[1])
-            k_indexes = t.get_nns_by_vector(
-                input_vector,
-                NUM_NEIGHBOR,
-                search_k=-1,
-                include_distances=False
-            )
-            # check cosine similarity
-            max_cos_sim = self.cosine_similarity(
-                input_vector,
-                np.array(t.get_item_vector(k_indexes[0]))
-            )
-            if max_cos_sim < SIMILARITY_THRESHOLD: 
-                kg_indexes.append(-1)
-            else:
-                # select node with max cosine similarity
-                kg_indexes.append(k_indexes[0])
-
-        return kg_indexes
-
-    def word_structure_matching(self, document_entities):
-        
-        return None
-    
-    def doc_entity_linking(self, document_entities):
-
-        emb_idx = self.vector_similarity(document_entities)
-        
